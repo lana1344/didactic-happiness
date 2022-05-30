94d7fb25e0afae5f2b9693d8311a133a98cee7c6
lana1344 <106545187+lana1344@users.noreply.github.com>
2022-05-30 12:38:38 -0700

    Update app.py

--- app.py
+++ app.py
@@ -126,5 +126,5 @@ def index():
         return render_template('form.html')
 
 if __name__ == "__main__":
-    app.run(debug = True)
-    #app.run(host='0.0.0.0', port=80)
+    #app.run(debug = True)
+    app.run(host='0.0.0.0', port=80)
